04d93f9f8955aa8bb8db3da3dd139fbe612d9831
Shaojung Lu <shaojung@gmail.com>
2017-10-16 10:31:44 +0800

    CountDown Timer

--- app/src/main/java/com/example/teacher/t2017101601/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017101601/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.teacher.t2017101601;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView);
+    }
+    public void click1(View v)
+    {
+        new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tv1.setText("s:" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                tv1.setText("done!");
+            }
+        }.start();
     }
 }
